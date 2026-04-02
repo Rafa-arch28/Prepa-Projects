0889449fe57aeff1333fb6a17cf2f3b87a6747cc
Rafael Antonio de la Fuente <rafael.dee@uanl.edu.mx>
2026-04-01 21:24:28 -0600

    No lo guarde :(

--- PROGRA1/Loops/gestion_cajas.cpp
+++ PROGRA1/Loops/gestion_cajas.cpp
@@ -1,3 +1,25 @@
+/* Objetivo: Desarrollar un programa en C++ que administre el flujo de efectivo y el conteo de 
+clientes de una tienda de informática con tres puntos de venta independientes.
+
+Requerimientos Funcionales:
+
+Ciclo Indefinido: El programa debe ejecutarse continuamente (do-while) ya que el número de 
+clientes diarios es desconocido.
+
+Interfaz de Usuario: Menú interactivo con 4 opciones (Caja 1, 2, 3 y Finalizar Turno).
+
+Captura de Datos: Para cada venta, el sistema debe registrar el monto en efectivo y el número de 
+clientes atendidos.
+
+Acumulación Selectiva: Los datos deben sumarse únicamente a la caja seleccionada mediante una 
+estructura de control (switch).
+
+Reporte de Cierre (Output Final):
+Al terminar el turno, el sistema debe generar un desglose detallado que incluya:
+
+Por Caja: Total de efectivo y total de clientes para la Caja 1, Caja 2 y Caja 3.
+
+Globales: Sumatoria total de efectivo recaudado y cantidad total de clientes atendidos en toda la tienda */
 
 
 #include <iostream>
@@ -71,7 +93,7 @@ int main() {
                 cout << "Clientes en Caja 3: " << clientes_caja3 << endl << endl;
 
                 cout << "CLIENTES TOTALES: " << clientes_totales << endl;
-                cout << "EFECTIVO TOTAL: " << efectivo_total;
+                cout << "EFECTIVO TOTAL: " << efectivo_total << endl;
                 break;
             
             default:
@@ -84,3 +106,24 @@ int main() {
 
     return 0;
 }
+
+/* Un ejercicio que me ayudo con mi desarrollo del PIA, fue increible que lograra que funcionara
+a la primera pero tenia errores de logica, por ejemplo el que mas perjudicaba el programa era este: 
+
+    case 1:
+
+        efectivo_caja1 += efectivo;
+        clientes_caja1 += clientes;
+
+        clientes_totales += clientes_caja1;
+        efectivo_total += efectivo_caja1;
+        break; 
+    
+Como pueden ver sumaba el efectivo de cada caja a el total directamente en el case, lo que hacia 
+que el total aumentara una vez de mas obviamente arruinando el calculo y algo de lo que estoy muy
+feliz fue que pude usar por primera vez el debugger de vscode para darme cuenta del error porque no
+lo encontraba, lo solucione cambiando el calculo total hasta case 4, donde se imprime el reporte final.
+
+Tambien si estuviste viendo mis codigos anteriores, pudiste darte cuenta que use la misma logica del 
+condicional para no tener que pedir los datos cada vez que entraba en el switch.
+*/
