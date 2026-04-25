9fff48356477b97406ece5562ec97f3d31df33bf
RAFAEL ANTONIO DE LA FUENTE ESPARZA <rafael.dee@uanl.edu.mx>
2026-04-24 23:31:35 -0600

    no se q es

--- PROGRA1/Loops/Trabajo Diario/tempCodeRunnerFile.cpp
+++ PROGRA1/Loops/Trabajo Diario/tempCodeRunnerFile.cpp
@@ -1,3 +1,23 @@
-for (int k = 0; k < 100; k++) {
-        cout << k + 1 << endl;
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int k = 0; k < 500; k++) {
+        cout << (k + 1) * 2 << endl;
     }
+    
+    /* int acumulador = 0;
+    while (acumulador < 500) {
+        cout << (acumulador + 1) * 2 << endl;
+        acumulador++;
+    } */
+
+    /* int acumulador = 0;
+    do {
+        cout << (acumulador + 1) * 2 << endl;
+        acumulador++;
+    } while (acumulador < 500); */
+    
+    return 0;
+}
