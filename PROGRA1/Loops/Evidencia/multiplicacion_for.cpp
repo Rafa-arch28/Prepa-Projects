001b4d15c030b7835d5d30bac93c79712b8bfda5
RAFAEL ANTONIO DE LA FUENTE ESPARZA <rafael.dee@uanl.edu.mx>
2026-04-28 08:57:35 -0600

    solo cambio de lugar el primer comentario

--- PROGRA1/Loops/Evidencia/multiplicacion_for.cpp
+++ PROGRA1/Loops/Evidencia/multiplicacion_for.cpp
@@ -1,10 +1,10 @@
 /* Realice un programa que imprima las tablas del 1 al 10 usando FOR */
 
 /* Principalmente aprendi a simplificar todo con un segundo loop,
- al principio pense en hacerlo poniendo pues cada tabla pero me di cuenta
- que era muuchisimo mas facil si usaba un segundo bucle, al principio tuve 
- errores porque usaba el array tablas[] en el segundo bucle pero era sin el
- asi que todo bien :) */
+al principio pense en hacerlo poniendo pues cada tabla pero me di cuenta
+que era muuchisimo mas facil si usaba un segundo bucle, al principio tuve 
+errores porque usaba el array tablas[] en el segundo bucle pero era sin el
+asi que todo bien :) */
 
 #include <iostream>
 using namespace std;
